b1d963019f9fbb9a19a2d9affbab90cd3ea8374f
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2016-12-03 19:12:43 -0500

    Failed attempt to use animator objects to move the top most view to the bottom

--- Presentation/ViewController.swift
+++ Presentation/ViewController.swift
@@ -18,6 +18,11 @@ func rootVC(currentVC: UIViewController) -> UIViewController {
 	return rootVC(currentVC: p)
 }
 
+func topVC(currentVC: UIViewController) -> UIViewController {
+	guard let p = currentVC.presentedViewController else { return currentVC }
+	return topVC(currentVC: p)
+}
+
 class ViewController: UIViewController {
 	@IBOutlet weak var level: UILabel!
 
@@ -39,21 +44,64 @@ class ViewController: UIViewController {
 	// As described in the above comment, you may not want that behavior and instead want the top most view controller to dismiss with animation whil all the ones in between remain invisible.
 	// Solution: you must move the view from the top view controller to the view controller of the presented view controller of the root.  But there is a cavet, if the top view has elements that connect to toplayout guide or bottom layout guide, then the views will shift.  the fix is to ensure that all view elements in the view don't use the top or bottom layout guide.
 	@IBAction func dismissAllSpecial(_ sender: Any) {
+//		let root = rootVC(currentVC: self)
+//		
+//		if let root1 = root.presentedViewController,
+//			root1 != self
+//		{
+//			let v = view!
+//			self.view = UIView()
+//			//v.backgroundColor = UIColor.red.withAlphaComponent(0.6)
+//			//root1.view.addSubview(v)
+//			root1.view = v
+//		}
+//		
+//		root.dismiss(animated: true)
+		
 		let root = rootVC(currentVC: self)
 		
 		if let root1 = root.presentedViewController,
 			root1 != self
 		{
-			let v = view!
-			self.view = UIView()
-			//v.backgroundColor = UIColor.red.withAlphaComponent(0.6)
-			//root1.view.addSubview(v)
-			root1.view = v
+			root1.transitioningDelegate = root as? UIViewControllerTransitioningDelegate
+			(root1 as! ViewController).lastTopVC = self
 		}
 		
 		root.dismiss(animated: true)
 	}
 	
+	var lastTopVC: UIViewController?
 }
 
+extension ViewController: UIViewControllerTransitioningDelegate {
+	func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+		return DismissAnimator()
+	}
+}
+
+class DismissAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+	func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+		return 0.5
+	}
+	func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+		let fromVC = transitionContext.viewController(forKey: .from)!
+		let top = (fromVC as! ViewController).lastTopVC!
+		let fromView = top.view!
+		top.view = UIView()
+		let toView = transitionContext.view(forKey: .to)!
+		let containerView = transitionContext.containerView
+		containerView.subviews.forEach({ $0.removeFromSuperview() })
+		
+		containerView.addSubview(toView)
+		containerView.addSubview(fromView)
+		
+		UIView.animate(withDuration: transitionDuration(using: transitionContext)
+			, animations: {
+				fromView.frame.offsetBy(dx: 0, dy: fromView.frame.height)
+		}, completion: { _ in
+			transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+		})
+
+	}
+}
 
